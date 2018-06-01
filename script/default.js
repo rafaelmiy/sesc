a56e94c46627f9891e477ebcda27739d1aa8146e
Rafaelmiy <rafaelmiy@gmail.com>
2018-06-01 19:36:45 -0300

    santo avanço

--- script/default.js
+++ script/default.js
@@ -21,3 +21,33 @@ $(document).on('change', '#check', function() {
     $('#background').removeClass('show');
   }
 });
+
+// $('#cultureTabs a').click(function (e) {
+//   e.preventDefault();
+//   $(this).tab('show');
+// });
+
+// cache the id
+var navbox = $('#cultureTabs');
+// activate tab on click
+navbox.on('click', 'a', function (e) {
+  var $this = $(this);
+  // prevent the Default behavior
+  e.preventDefault();
+  // set the hash to the address bar
+  window.location.hash = $this.attr('href');
+  // activate the clicked tab
+  $this.tab('show');
+});
+
+function refreshHash(){
+    navbox.find('a[href="'+window.location.hash+'"]').tab('show');
+}
+
+// You might need a polyfill for this event
+$(window).bind('hashchange', refreshHash);
+if(window.location.hash){
+  // show right tab on load (read hash from address bar)
+  refreshHash();
+}
+$('.dropdown-toggle').dropdown();
