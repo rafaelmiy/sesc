9ddb80c0017ca8ea512dd46fad72bd50087c62cc
Rafaelmiy <rafaelmiy@gmail.com>
2018-06-03 16:23:38 -0300

    up

--- script/default.js
+++ script/default.js
@@ -1,18 +1,3 @@
-//$('[data-paroller-factor]').paroller();
-// $('.paroller').paroller({
-//   factor: 0.4,
-//   type: 'foreground'
-// });
-
-//  $('body').scrollspy({target: '#main-navbar'});
-
-// $('.center').slick({
-//   dots: false,
-//   centerMode: true,
-//   centerPadding: '90px',
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-// });
 
 $(document).on('change', '#check', function() {
   if(this.checked) {
@@ -22,21 +7,11 @@ $(document).on('change', '#check', function() {
   }
 });
 
-// $('#cultureTabs a').click(function (e) {
-//   e.preventDefault();
-//   $(this).tab('show');
-// });
-
-// cache the id
 var navbox = $('#cultureTabs');
-// activate tab on click
 navbox.on('click', 'a', function (e) {
   var $this = $(this);
-  // prevent the Default behavior
   e.preventDefault();
-  // set the hash to the address bar
   window.location.hash = $this.attr('href');
-  // activate the clicked tab
   $this.tab('show');
 });
 
@@ -44,10 +19,7 @@ function refreshHash(){
     navbox.find('a[href="'+window.location.hash+'"]').tab('show');
 }
 
-// You might need a polyfill for this event
 $(window).bind('hashchange', refreshHash);
 if(window.location.hash){
-  // show right tab on load (read hash from address bar)
   refreshHash();
 }
-$('.dropdown-toggle').dropdown();
